9ea8d4826755cae5225cad2a2717ebdf10741cc0
Michael Day <github@mday.ca>
2025-08-16 13:54:59 -0700

    add set character class chat command

---
+++ Assets/AnyRPG/Core/System/Scripts/ChatCommand/SetCharacterClassCommand.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace AnyRPG {
+    [CreateAssetMenu(fileName = "New Set CharacterClass Command", menuName = "AnyRPG/Chat Commands/Set CharacterClass Command")]
+    public class SetCharacterClassCommand : ChatCommand {
+
+        [Header("Set Character Class Command")]
+
+        [Tooltip("If true, all parameters will be ignored, and the character class will be the one listed below")]
+        [SerializeField]
+        private bool fixedCharacterClass = false;
+
+        [Tooltip("The name of the character class to set")]
+        [SerializeField]
+        [ResourceSelector(resourceType = typeof(CharacterClass))]
+        private string characterClassName = string.Empty;
+
+        private CharacterClass characterClass = null;
+
+        public override void ExecuteCommand(string commandParameters, int accountId) {
+            //Debug.Log("SetCharacterClassCommand.ExecuteCommand() Executing command " + DisplayName + " with parameters (" + commandParameters + ")");
+
+            // set the fixed characterClass
+            if (fixedCharacterClass == true && characterClass != null) {
+                SetCharacterClass(characterClass, accountId);
+                return;
+            }
+
+            // the characterClass comes from parameters, but none were given
+            if (commandParameters == string.Empty) {
+                return;
+            }
+
+            characterClass = systemDataFactory.GetResource<CharacterClass>(characterClassName);
+            if (characterClass == null) {
+                return;
+            }
+            SetCharacterClass(characterClass, accountId);
+        }
+
+        private void SetCharacterClass(CharacterClass characterClass, int accountId) {
+            Debug.Log($"SetCharacterClassCommand.SetCharacterClass({accountId})");
+            playerManagerServer.SetPlayerCharacterClass(characterClass, accountId);
+        }
+
+        public override void SetupScriptableObjects(SystemGameManager systemGameManager) {
+            base.SetupScriptableObjects(systemGameManager);
+
+            if (characterClassName != string.Empty) {
+                characterClass = systemDataFactory.GetResource<CharacterClass>(characterClassName);
+                if (characterClass == null) {
+                    Debug.LogError($"SetCharacterClassCommand.SetupScriptableObjects(): Could not find characterClass {characterClassName} for command {ResourceName}");
+                }
+            }
+        }
+    }
+
+}
